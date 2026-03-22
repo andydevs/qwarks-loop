21b141f4ea54371ab5ed85490148314e13e80370
Anshul Kharbanda <akanshul97@gmail.com>
2026-03-22 23:14:08 +0000

    Refactor webpack configuration to allow livereload of wasm libraries while devserver is active

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,16 +2,30 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
-  },
-  mode: "development",
-  plugins: [
-    new CopyWebpackPlugin({ patterns: ['index.html'] })
-  ],
-  experiments: {
-    asyncWebAssembly: true
-  }
+    mode: "development",
+    entry: "./bootstrap.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bootstrap.js",
+    },
+    watchOptions: {
+        aggregateTimeout: 200,
+        poll: 1000
+    },
+    devServer: {
+        hot: false,
+        liveReload: true,
+        watchFiles: [
+            path.resolve(__dirname, "*.js"),
+            path.resolve(__dirname, "*.html"),
+            path.resolve(__dirname, "../pkg/*.js"),
+            path.resolve(__dirname, "../pkg/*.wasm")
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin({ patterns: ['index.html'] })
+    ],
+    experiments: {
+        asyncWebAssembly: true
+    },
 };
